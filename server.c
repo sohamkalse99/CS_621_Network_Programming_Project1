98f9f74140a0a2de12ea388516413feb84405755
vboxuser <vboxuser@Client.myguest.virtualbox.org>
2023-03-19 14:13:32 -0700

    Updated server.c file

--- server.c
+++ server.c
@@ -8,19 +8,27 @@
 #include <unistd.h>
 #include "cJSON.c"
 #include "cJSON.h"
-
+#include <stdbool.h>
+#include <sys/time.h>
+#include <signal.h>
 // #define PORT 8080
 #define SA struct sockaddr
-
+int flag = 0;
 struct config_file{
-    char serverIP[50];
-    char sourcePortUDP[10];
-    char destPortUDP[10];
-    char portTCP[10];
+    char server_ip[50];
+    char source_port_udp[10];
+    char dest_port_udp[10];
+    char port_tcp[10];
+    char payload[50];
+    char inter_measure_time[50];
+    char no_of_packets[50];
+    char ttl[50];
 };
 
+struct config_file* config_file;
+
 char* get_client_data(int connfd){
-    char buffer[1024];
+    char buffer[2000];
 
     //char* buffer = malloc(sizeof(*buffer));
 
@@ -61,10 +69,17 @@ char* get_client_data(int connfd){
         }
     }*/
 
+    bzero(buffer, sizeof(buffer));
     read(connfd, buffer, sizeof(buffer));// read message from client and store it in buffer
 
+    
     char* ptr = malloc(strlen(buffer)+1);
     strcpy(ptr, buffer);
+    //printf("%s",buffer);
+    bzero(buffer, sizeof(buffer));
+    strcpy(buffer, "Server received Config file");
+    write(connfd, buffer, sizeof(buffer));
+
     return ptr;
     // cJSON *json = cJSON_Parse(buffer);
     
@@ -81,38 +96,53 @@ char* get_client_data(int connfd){
     
 }
 
-void create_json(char* buffer){
+bool create_json(char* buffer){
 
     cJSON *json = cJSON_Parse(buffer);
-    
+    bool json_created = true;
     if(json == NULL){
         printf("---------------");
         printf("Error parsing json string:%s\n", cJSON_GetErrorPtr());
         printf("---------------");
+        json_created = false;
         exit(1);
     }
 
-    const cJSON *serverIP = cJSON_GetObjectItemCaseSensitive(json, "serverIP");
+    const cJSON *server_ip = cJSON_GetObjectItemCaseSensitive(json, "serverIP");
+    const cJSON *source_port_udp = cJSON_GetObjectItemCaseSensitive(json, "sourcePortUDP");
+    const cJSON *dest_port_udp = cJSON_GetObjectItemCaseSensitive(json, "destPortUDP");
+    const cJSON *port_tcp = cJSON_GetObjectItemCaseSensitive(json, "portTCP");
+    const cJSON *payload = cJSON_GetObjectItemCaseSensitive(json, "payload");
+    const cJSON *inter_measure_time = cJSON_GetObjectItemCaseSensitive(json, "interMeasureTime");
+    const cJSON *no_of_packets = cJSON_GetObjectItemCaseSensitive(json, "noOfPackets");
+    const cJSON *ttl = cJSON_GetObjectItemCaseSensitive(json, "TTL");
+
+
     // printf("Server IP = %s", serverIP->valuestring);
 
-    struct config_file config_file;
-    strcpy(config_file.serverIP, serverIP->valuestring);
-    printf("Server IP = %s", config_file.serverIP);
+    // struct config_file config_file;
+    strcpy(config_file->server_ip, server_ip->valuestring);
+    strcpy(config_file->source_port_udp, source_port_udp->valuestring);
+    strcpy(config_file->dest_port_udp, dest_port_udp->valuestring);
+    strcpy(config_file->port_tcp, port_tcp->valuestring);
+    strcpy(config_file->payload, payload->valuestring);
+    strcpy(config_file->inter_measure_time, inter_measure_time->valuestring);
+    strcpy(config_file->no_of_packets, no_of_packets->valuestring);
+    strcpy(config_file->ttl, ttl->valuestring);
+
+    printf("Server IP = %s", config_file->server_ip);
+
+    return json_created;
 }
 
-void main(int argc, char **argv){
-
-    if(argc<2){
-        printf("Please provide command line arguments");
-        return;
-    }
+bool tcp_connection(char* cmd_line_arg){
 
     int sockfd, connfd, len;
     struct sockaddr_in servaddr, cli;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);//ipv4/6, connection_oriented/connection_less, protocol
 
-    if(sockfd<0){
+    if(sockfd == -1){
         printf("Error in socket creation\n");
         exit(0);
     }else{
@@ -123,7 +153,7 @@ void main(int argc, char **argv){
 
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY); //assigns s_addr = 0.0.0.0
-    int port = atoi(argv[1]);
+    int port = atoi(cmd_line_arg);
     servaddr.sin_port = htons(port);
 
     if((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0){
@@ -136,7 +166,7 @@ void main(int argc, char **argv){
         printf("Listen Failed");
         exit(0);
     }else
-        printf("Server Listening");
+        printf("Server Listening\n");
 
     int len_cli = sizeof(cli);
 
@@ -148,9 +178,14 @@ void main(int argc, char **argv){
     }else
         printf("Client accepted by the server\n");
 
+    
+    // bool isConnected =false;
     char* buffer = get_client_data(connfd);
 
-    create_json(buffer);
+    // if(sizeof(buffer)>0)
+    //     isConnected = true;
+    
+    bool json_created = create_json(buffer);
     // cJSON *json = cJSON_Parse(buffer);
     
     // const cJSON *serverIP = cJSON_GetObjectItem(json, "serverIP");
@@ -159,4 +194,138 @@ void main(int argc, char **argv){
 
     close(sockfd);
 
+    return json_created;
+}
+
+
+
+
+
+void udp_connection(){
+    char buffer[100];
+    char* message = "Hello Client";
+    int listenfd, len;
+    struct sockaddr_in serv_addr, cli_addr;
+    bzero(&serv_addr, sizeof(serv_addr));
+
+    listenfd = socket(AF_INET, SOCK_DGRAM, 0);//UDP Socket
+
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    int port = atoi(config_file->dest_port_udp);
+    serv_addr.sin_port = htons(port);
+    serv_addr.sin_family = AF_INET;
+
+    
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    len = sizeof(cli_addr);
+    
+    
+    int n = recvfrom(listenfd, buffer, sizeof(buffer), MSG_WAITALL, (struct sockaddr*)&cli_addr, &len);
+    buffer[n] = '\0';
+    printf("%s", buffer);
+    
+
+    //sendto(listenfd, message, 1000, 0, (struct sockaddr*)&cli_addr, sizeof(cli_addr));
+}
+
+void sig_handler(int sig_num){
+    printf("loss");
+    flag =1;
+    //exit(0);
+}
+
+void low_entropy(int listenfd, struct sockaddr_in cli_addr,int len){
+
+    struct timeval t1, t2;
+
+    // int packet[998];
+    int packet[atoi(config_file->payload)];
+    gettimeofday(&t1, NULL);
+    signal(SIGALRM, sig_handler);
+
+    for(int i =0;i<atoi(config_file->no_of_packets) && flag == 0;i++){
+        //printf("i = %d\n",i);
+        
+        
+        int n = recvfrom(listenfd, packet, sizeof(packet), MSG_WAITALL, (struct sockaddr*)&cli_addr, &len);
+
+        if(i==0){
+            alarm(15);
+        }
+
+        //printf("Packet->%d\n", i);
+        packet[n] = '\0';
+        gettimeofday(&t2, NULL);
+    }
+
+    //printf("Outside for loop");
+    long int time_diff = (t2.tv_sec - t1.tv_sec)*1000000+(t2.tv_usec - t1.tv_usec);
+    printf("Time difference of low entropy data is %ld", time_diff);
+}
+
+void high_entropy(int listenfd, struct sockaddr_in cli_addr,int len){
+    struct timeval t1, t2;
+
+    char packet[atoi(config_file->payload)];
+    gettimeofday(&t1, NULL);
+    flag = 0;//Check whether this will work 
+    signal(SIGALRM, sig_handler);
+
+    for(int i =0;i<atoi(config_file->no_of_packets) && flag == 0;i++){
+        //printf("i = %d\n",i);
+        int n = recvfrom(listenfd, packet, sizeof(packet), MSG_WAITALL, (struct sockaddr*)&cli_addr, &len);
+        // printf("Packet->%d\n", i);
+
+        if(i==0){
+            alarm(15);
+        }
+
+        packet[n] = '\0';
+        gettimeofday(&t2, NULL);
+    }
+
+    long int time_diff = (t2.tv_sec - t1.tv_sec)*1000000+(t2.tv_usec - t1.tv_usec);
+    printf("Time difference of high entropy data is %ld", time_diff);
+}
+void udp_packets(){
+
+    int listenfd, len;
+    struct sockaddr_in serv_addr, cli_addr;
+    bzero(&serv_addr, sizeof(serv_addr));
+
+    listenfd = socket(AF_INET, SOCK_DGRAM, 0);//UDP Socket
+
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    int port = atoi(config_file->dest_port_udp);
+    serv_addr.sin_port = htons(port);
+    serv_addr.sin_family = AF_INET;
+
+    
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    len = sizeof(cli_addr);
+
+    low_entropy(listenfd, cli_addr,len);
+
+    
+    //high_entropy(listenfd, cli_addr,len);
+
+}
+
+void main(int argc, char **argv){
+
+    config_file = malloc(sizeof(*config_file));
+
+    if(argc<2){
+        printf("Please provide command line arguments");
+        return;
+    }
+
+    // bool config_received = 
+    bool json_created = tcp_connection(argv[1]);
+    // printf("Dest Port -> %s\n", config_file->dest_port_udp);
+    //udp_connection();
+
+    udp_packets();
 }

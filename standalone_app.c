f95e23ac199129c012aa671994564deafa371995
vboxuser <vboxuser@Client.myguest.virtualbox.org>
2023-03-24 15:24:50 -0700

    Added both low as well as high entropy data along with  head and tail SYN packets

--- standalone_app.c
+++ standalone_app.c
@@ -594,7 +594,7 @@ allocate_intmem (int len) {
 }
 
 
-int udp_packets(){
+int udp_low_packets(){
   int i, status, datalen, frame_length, sd, bytes, *ip_flags;
   char *interface, *target, *src_ip, *dst_ip;
   struct ip iphdr;
@@ -837,11 +837,275 @@ int udp_packets(){
   return (EXIT_SUCCESS);
 }
 
+int udp_high_packets(){
+  
+  int i, status, datalen, frame_length, sd, bytes, *ip_flags;
+  char *interface, *target, *src_ip, *dst_ip;
+  struct ip iphdr;
+  struct udphdr udphdr;
+  uint8_t *data, *src_mac, *dst_mac, *ether_frame;
+  struct addrinfo hints, *res;
+  struct sockaddr_in *ipv4;
+  struct sockaddr_ll device;
+  struct ifreq ifr;
+  void *tmp;
+
+  // Allocate memory for various arrays.
+  src_mac = allocate_ustrmem (6);
+  dst_mac = allocate_ustrmem (6);
+  data = allocate_ustrmem (IP_MAXPACKET);
+  ether_frame = allocate_ustrmem (IP_MAXPACKET);
+  interface = allocate_strmem (40);
+  target = allocate_strmem (40);
+  src_ip = allocate_strmem (INET_ADDRSTRLEN);
+  dst_ip = allocate_strmem (INET_ADDRSTRLEN);
+  ip_flags = allocate_intmem (4);
+
+  // Interface to send packet through.
+  strcpy (interface, "enp0s3");
+
+  // Submit request for a socket descriptor to look up interface.
+  if ((sd = socket (PF_PACKET, SOCK_RAW, htons (ETH_P_ALL))) < 0) {
+    perror ("socket() failed to get socket descriptor for using ioctl() ");
+    exit (EXIT_FAILURE);
+  }
+
+  // Use ioctl() to look up interface name and get its MAC address.
+  memset (&ifr, 0, sizeof (ifr));
+  snprintf (ifr.ifr_name, sizeof (ifr.ifr_name), "%s", interface);
+  if (ioctl (sd, SIOCGIFHWADDR, &ifr) < 0) {
+    perror ("ioctl() failed to get source MAC address ");
+    return (EXIT_FAILURE);
+  }
+  close (sd);
+
+  // Copy source MAC address.
+  memcpy (src_mac, ifr.ifr_hwaddr.sa_data, 6 * sizeof (uint8_t));
+
+  // Report source MAC address to stdout.
+  printf ("MAC address for interface %s is ", interface);
+  for (i=0; i<5; i++) {
+    printf ("%02x:", src_mac[i]);
+  }
+  printf ("%02x\n", src_mac[5]);
+
+  // Find interface index from interface name and store index in
+  // struct sockaddr_ll device, which will be used as an argument of sendto().
+  memset (&device, 0, sizeof (device));
+  if ((device.sll_ifindex = if_nametoindex (interface)) == 0) {
+    perror ("if_nametoindex() failed to obtain interface index ");
+    exit (EXIT_FAILURE);
+  }
+  printf ("Index for interface %s is %i\n", interface, device.sll_ifindex);
+
+  // Set destination MAC address: you need to fill these out
+  dst_mac[0] = 0x08;
+  dst_mac[1] = 0x00;
+  dst_mac[2] = 0x27;
+  dst_mac[3] = 0x7b;
+  dst_mac[4] = 0xb8;
+  dst_mac[5] = 0x2d;
+
+  // Source IPv4 address: you need to fill this out
+  strcpy (src_ip, "10.0.0.245");
+
+  // Destination URL or IPv4 address: you need to fill this out
+  strcpy (target, "10.0.0.27");
+
+  // Fill out hints for getaddrinfo().
+  memset (&hints, 0, sizeof (struct addrinfo));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = hints.ai_flags | AI_CANONNAME;
+
+  // Resolve target using getaddrinfo().
+  if ((status = getaddrinfo (target, NULL, &hints, &res)) != 0) {
+    fprintf (stderr, "getaddrinfo() failed for target: %s\n", gai_strerror (status));
+    exit (EXIT_FAILURE);
+  }
+  ipv4 = (struct sockaddr_in *) res->ai_addr;
+  tmp = &(ipv4->sin_addr);
+  if (inet_ntop (AF_INET, tmp, dst_ip, INET_ADDRSTRLEN) == NULL) {
+    status = errno;
+    fprintf (stderr, "inet_ntop() failed for target.\nError message: %s", strerror (status));
+    exit (EXIT_FAILURE);
+  }
+  freeaddrinfo (res);
+
+  // Fill out sockaddr_ll.
+  device.sll_family = AF_PACKET;
+  memcpy (device.sll_addr, src_mac, 6 * sizeof (uint8_t));
+  device.sll_halen = 6;
+
+  // UDP data
+  // datalen = 4;
+  // data[0] = 'T';
+  // data[1] = 'e';
+  // data[2] = 's';
+  // data[3] = 't';
+
+  data[atoi(config_file->payload)];
+  
+  datalen = atoi(config_file->payload);
+
+  FILE *fp = fopen("HighEntropyData","r");
+
+  if(fp == NULL){
+      perror("Failed to open file");
+  }
+
+  fgets(data, atoi(config_file->payload), fp);
+
+  // IPv4 header
+
+  // IPv4 header length (4 bits): Number of 32-bit words in header = 5
+  iphdr.ip_hl = IP4_HDRLEN / sizeof (uint32_t);
+
+  // Internet Protocol version (4 bits): IPv4
+  iphdr.ip_v = 4;
+
+  // Type of service (8 bits)
+  iphdr.ip_tos = 0;
+
+  // Total length of datagram (16 bits): IP header + UDP header + datalen
+  iphdr.ip_len = htons (IP4_HDRLEN + UDP_HDRLEN + datalen);
+
+  // ID sequence number (16 bits): unused, since single datagram
+  iphdr.ip_id = htons (0);
+
+  // Flags, and Fragmentation offset (3, 13 bits): 0 since single datagram
+
+  // Zero (1 bit)
+  ip_flags[0] = 0;
+
+  // Do not fragment flag (1 bit)
+  ip_flags[1] = 1;
+
+  // More fragments following flag (1 bit)
+  ip_flags[2] = 0;
+
+  // Fragmentation offset (13 bits)
+  ip_flags[3] = 0;
+
+  iphdr.ip_off = htons ((ip_flags[0] << 15)
+                      + (ip_flags[1] << 14)
+                      + (ip_flags[2] << 13)
+                      +  ip_flags[3]);
+
+  // Time-to-Live (8 bits): default to maximum value
+  iphdr.ip_ttl = atoi(config_file->ttl);
+
+  // Transport layer protocol (8 bits): 17 for UDP
+  iphdr.ip_p = IPPROTO_UDP;
+
+  // Source IPv4 address (32 bits)
+  if ((status = inet_pton (AF_INET, src_ip, &(iphdr.ip_src))) != 1) {
+    fprintf (stderr, "inet_pton() failed for source address.\nError message: %s", strerror (status));
+    exit (EXIT_FAILURE);
+  }
+
+  // Destination IPv4 address (32 bits)
+  if ((status = inet_pton (AF_INET, dst_ip, &(iphdr.ip_dst))) != 1) {
+    fprintf (stderr, "inet_pton() failed for destination address.\nError message: %s", strerror (status));
+    exit (EXIT_FAILURE);
+  }
+
+  // IPv4 header checksum (16 bits): set to 0 when calculating checksum
+  iphdr.ip_sum = 0;
+  iphdr.ip_sum = checksum ((uint16_t *) &iphdr, IP4_HDRLEN);
+
+  // UDP header
+
+  // Source port number (16 bits): pick a number
+  udphdr.source = htons (4950);
+
+  // Destination port number (16 bits): pick a number
+  udphdr.dest = htons (4950);
+
+  // Length of UDP datagram (16 bits): UDP header + UDP data
+  udphdr.len = htons (UDP_HDRLEN + datalen);
+
+  // UDP checksum (16 bits)
+  udphdr.check = udp4_checksum (iphdr, udphdr, data, datalen);
+
+  // Fill out ethernet frame header.
+
+  // Ethernet frame length = ethernet header (MAC + MAC + ethernet type) + ethernet data (IP header + UDP header + UDP data)
+  frame_length = 6 + 6 + 2 + IP4_HDRLEN + UDP_HDRLEN + datalen;
+
+  // Destination and Source MAC addresses
+  memcpy (ether_frame, dst_mac, 6 * sizeof (uint8_t));
+  memcpy (ether_frame + 6, src_mac, 6 * sizeof (uint8_t));
+
+  // Next is ethernet type code (ETH_P_IP for IPv4).
+  // http://www.iana.org/assignments/ethernet-numbers
+  ether_frame[12] = ETH_P_IP / 256;
+  ether_frame[13] = ETH_P_IP % 256;
+
+  // Next is ethernet frame data (IPv4 header + UDP header + UDP data).
+
+  // IPv4 header
+  memcpy (ether_frame + ETH_HDRLEN, &iphdr, IP4_HDRLEN * sizeof (uint8_t));
+
+  // UDP header
+  memcpy (ether_frame + ETH_HDRLEN + IP4_HDRLEN, &udphdr, UDP_HDRLEN * sizeof (uint8_t));
+
+
+  // Submit request for a raw socket descriptor.
+  if ((sd = socket (PF_PACKET, SOCK_RAW, htons (ETH_P_ALL))) < 0) {
+    perror ("socket() failed ");
+    exit (EXIT_FAILURE);
+  }
+
+  for(int i =0;i<atoi(config_file->no_of_packets);i++){
+    
+    data[0] = (i>>8) & 0xFF;
+    data[1] = i & 0xFF;
+
+    // UDP data
+    memcpy (ether_frame + ETH_HDRLEN + IP4_HDRLEN + UDP_HDRLEN, data, datalen * sizeof (uint8_t));
+    
+    // Send ethernet frame to socket.
+    if ((bytes = sendto (sd, ether_frame, frame_length, 0, (struct sockaddr *) &device, sizeof (device))) <= 0) {
+      perror ("sendto() failed");
+      exit (EXIT_FAILURE);
+    }
+
+  }
+
+  // Close socket descriptor.
+  close (sd);
+
+  // Free allocated memory.
+  free (src_mac);
+  free (dst_mac);
+  free (data);
+  free (ether_frame);
+  free (interface);
+  free (target);
+  free (src_ip);
+  free (dst_ip);
+  free (ip_flags);
+
+  return (EXIT_SUCCESS);
+
+}
 
 void part1(){
   
-  int exit_success = syn_packet(atoi(config_file->dest_port_tcp_head));
-  udp_packets();
+  int exit_success_head1 = syn_packet(atoi(config_file->dest_port_tcp_head));
+  udp_low_packets();
+
+  int exit_success_tail1 = syn_packet(atoi(config_file->dest_port_tcp_tail));
+
+  sleep(atoi(config_file->inter_measure_time));
+
+  int exit_success_head2 = syn_packet(atoi(config_file->dest_port_tcp_head));
+
+  udp_high_packets();
+
+  int exit_success_tail2 = syn_packet(atoi(config_file->dest_port_tcp_tail));
+
 }
 
 
